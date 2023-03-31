aa88f4b625bd00621170f49818f52b8307994f37
Albi <115578672+albiiroad@users.noreply.github.com>
2023-03-31 17:38:18 +0200

    Add files via upload

--- TDD Practica Coche/tests/TestCoche.java
+++ TDD Practica Coche/tests/TestCoche.java
@@ -11,7 +11,7 @@ public class TestCoche {
     @Test
     public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_Alba_Melgar(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
@@ -19,7 +19,7 @@ public class TestCoche {
     public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerar_Alba_Melgar(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
@@ -27,7 +27,7 @@ public class TestCoche {
     public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_Alba_Melgar(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
